734ab9bb1f0052cec5d4e7524280b7bb9961b157
JoseAgarcia2019 <20199056@itla.edu.do>
2020-06-17 03:35:53 +0000

    new

---
+++ ejercicios/ejercicio1.cs
@@ -0,0 +1,28 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		             //table del 7
+	int count = 1;
+		int  tabl = 7;
+		int  res =  0;
+		
+		Console.WriteLine("Aqui se monstrara la tabla del 7");
+		
+		while (count <= 12){
+		
+		  res = tabl*count;
+			
+		Console.WriteLine(tabl+" x "+count+" = "+res);	
+		 
+	    count = count + 1;		
+               }
+
+
+
+		
+
+	}
+}
