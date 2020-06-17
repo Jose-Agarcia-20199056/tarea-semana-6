71a0b125b0a887dc8ad252e5087b4a7f7416de44
JoseAgarcia2019 <20199056@itla.edu.do>
2020-06-17 03:47:05 +0000

    new3

---
+++ ejercicios/ejercicio3.cs
@@ -0,0 +1,35 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{// numeros primos
+        // cantidad de digitos
+int num,dg = 0;
+		Console.WriteLine("Escriba un numero positivo please");
+		num = int.Parse(Console.ReadLine());
+	 
+	    if(num < 0){
+			
+			Console.WriteLine("Ingrese un numero positivo please");
+		  }
+		
+		
+		  
+			
+		else{
+			
+			do
+			{
+			 num= num/10;
+			  dg = dg + 1; 	 
+			} while ( num > 0);
+			 
+			Console.WriteLine(" El numero de digitos es {0}",dg );
+			
+			}
+
+
+
+	}
+}
