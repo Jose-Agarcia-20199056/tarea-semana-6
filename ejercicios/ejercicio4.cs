73f00b904c0e8178fbebfd2ddcf7dfed9537db7e
JoseAgarcia2019 <20199056@itla.edu.do>
2020-06-17 03:49:48 +0000

    fix

--- ejercicios/ejercicio4.cs
+++ ejercicios/ejercicio4.cs
@@ -11,7 +11,7 @@ public class Program
         while(r != "s"){
 		int n = random.Next(50, 100);
 		
-		Console.WriteLine("numeros random detectados : "+ n.ToString());
+		Console.WriteLine("numero random detectado: "+ n.ToString());
 		Console.WriteLine("pulse cualquier letra para generar otro numero random o 'S' = Salir");
 		r= Console.ReadLine();	
 		}
